a140b13bb2647c606aac18a033bdedfc7929857b
Bala Luguja <bluguja@gmail.com>
2020-04-05 01:47:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,12 +52,13 @@ def monopoly_with_third_tier
 end
 
 #binding.pry
+def monopoly_with_fourth_tier
 
 monopoly =  {
-  :railroads=>
+  :railroads =>
   {
-    :pieces=>4,
-    :rent_in_dollars=>
+    :pieces =>4,
+    :rent_in_dollars =>
     {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
@@ -66,22 +67,24 @@ monopoly =  {
     },
     :names=>
     {
-      :reading_railroad=>
+      :reading_railroad =>
       {
         "mortgage_value"=>"$100"
       },
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad =>
       {
         "mortgage_value"=>"$200"
       },
-      :b_and_o_railroad=>
+      :b_and_o_railroad =>
       {
         "mortgage_value"=>"$400"
       },
-      :shortline=>
+      :shortline =>
       {
         "mortgage_value"=>"$800"
       }
     }
   }
 }
+
+end
